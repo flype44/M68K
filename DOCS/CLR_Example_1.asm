aeb665595e24024b0e877a74922a214e8f48391c
Philippe CARPENTIER <philippe.carpentier.imei@gmail.com>
2015-09-27 09:27:37 +0200

    Removed some files to ignore

--- DOCS/CLR_Example_1.asm
+++
@@ -1,9 +0,0 @@
-; M68K
-; CLR Dn
-; Example 1
-
-START:	MOVE.L	#%11223344,D0 ; 11223344 
-		CLR.B	D0            ; 112233-- 
-		CLR.W	D0            ; 1122---- 
-		CLR.L	D0            ; -------- 
-EXIT:	STOP	#-1           ; 00000000
