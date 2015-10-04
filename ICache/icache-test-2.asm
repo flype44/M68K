4ad08d35214f164ead2d77f58943e58078c92ac6
Philippe CARPENTIER <philippe.carpentier.imei@gmail.com>
2015-10-04 23:08:02 +0200

    This testcase found a bug in v2626 to v2631

--- ICache/icache-test-2.asm
+++ ICache/icache-test-2.asm
@@ -7,7 +7,7 @@
 ; 2015-10-04 21:17:50
 ; Apollo Core Version : 2626 to 2631
 ; One bug revealed in ICache.
-; 21:17:50 BigGun Flype : your testcase 2 triggers a FAILURE in Icache.
+; 21:17:50 BigGun Flype : your testcase 2 triggers a FAILURE in ICache.
 ; 21:18:17 BigGun Only problem is that it takes like 1 hour to sim 
 ; 21:27:29 BigGun If we find bug this is good. 
 ; 21:27:41 BigGun As soon as I see bug in sim - we can quickly fix it 
@@ -24,8 +24,8 @@
 
 ;==========================================================
 
-size  equ 10  ; Number of bytes in routines
-count equ $50 ; Number of times we copy the routine
+size  equ 10    ; Number of bytes in routines
+count equ $1987 ; Number of times we copy the routine
 
 assert_zero equ $00D0000C
 
@@ -80,7 +80,6 @@ InsideRoutine2:
     divu.l  d0,d1:d2
     addq    #1,d5
     rts
-    ds.b    $4000
 CodeAHolder:
     ds.b    (size*count)
 CodeBHolder:
@@ -88,13 +87,13 @@ CodeBHolder:
 Exit:
     add.l   d1,d0
     add.l   d2,d0
-    sub.l   #$50e40a17,d0
+    sub.l   #$d95a3d60,d0
     move.l  d0,assert_zero
-    sub.l   #$00013c48,d1
+    sub.l   #$00206198,d1
     move.l  d6,assert_zero
-    sub.l   #$50e2062b,d2
+    sub.l   #$d9394ab0,d2
     move.l  d6,assert_zero
-    sub.l   #$a0,d5
+    sub.l   #$330e,d5
     move.l  d6,assert_zero
     sub.l   #$0000ffff,d6
     move.l  d6,assert_zero
