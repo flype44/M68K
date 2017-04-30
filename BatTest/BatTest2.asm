fe8c75c77b186039997b4bde3dcc0b266593e474
flype44 <flype44@gmail.com>
2017-04-30 19:34:52 +0200

    Update BatTest2.asm
    Added dummy sprites initialization.

--- BatTest/BatTest2.asm
+++ BatTest/BatTest2.asm
@@ -1,8 +1,8 @@
 ;==============================================================================
 ; Program: BatTest2.asm
-; Short:   Test the chipset planars and copper
-; Requir:  Vampire with core V3_PLANAR_xx.jic or better
-; Author:  APOLLO-Team, flype. 2017-April
+; Short:   Show a bat on an OCS screen
+; Requir:  Vampire with core V3_PLANAR_xx.jic
+; Author:  APOLLO-Team, flype. 2017-Apr.
 ;==============================================================================
 
     INCLUDE dos/dos.i
@@ -117,6 +117,19 @@ MAIN:
 .setdepth
     lea       CN0,a0                       ; Load Copper BPLCON0
     move.w    #DEPTH*$1000,2(a0)           ; Update Depth
+;    move.w    #$0010,2(a0)                 ; Update Depth
+
+.setprites
+    lea       _USERSPRITE1,a0              ; Load Bitmap
+    lea       S1H,a1                       ; Load Copper SPRxPTR
+    moveq.l   #8-1,d1                      ; Number of sprites to set
+.dosprite
+    move.l    a0,d0                        ; Load sprite
+    move.w    d0,6(a1)                     ; dc.w SPRxPTH,PTH
+    swap      d0                           ; Swap Low <=> High
+    move.w    d0,2(a1)                     ; dc.w SPRxPTL,PTL
+    addq.l    #8,a1                        ; Next SPRxPTR
+    dbf       d1,.dosprite                 ; Next sprite
 
 .setcopper
     lea       _USERCOPPER,a0               ; Set Copperlist
@@ -222,6 +235,7 @@ _INTNAME:      INTNAME
 
     EVEN
 _USERCOPPER:
+    ;
     dc.w      $0106,$0000   ; BPLCON3
     dc.w      $0180,$0000   ; COLOR00
     dc.w      $0182,$0002   ; COLOR01
@@ -231,34 +245,31 @@ _USERCOPPER:
     dc.w      $018a,$000A   ; COLOR05
     dc.w      $018c,$000C   ; COLOR06
     dc.w      $018e,$000E   ; COLOR07
-
-    dc.w      $0190,$0000   ; COLOR00
-    dc.w      $0192,$0020   ; COLOR01
-    dc.w      $0194,$0040   ; COLOR02
-    dc.w      $0196,$0060   ; COLOR03
-    dc.w      $0198,$0080   ; COLOR04
-    dc.w      $019a,$00A0   ; COLOR05
-    dc.w      $019c,$00C0   ; COLOR06
-    dc.w      $019e,$00E0   ; COLOR07
-    
-    dc.w      $01a0,$0000   ; COLOR00
-    dc.w      $01a2,$0200   ; COLOR01
-    dc.w      $01a4,$0400   ; COLOR02
-    dc.w      $01a6,$0600   ; COLOR03
-    dc.w      $01a8,$0800   ; COLOR04
-    dc.w      $01aa,$0A00   ; COLOR05
-    dc.w      $01ac,$0C00   ; COLOR06
-    dc.w      $01ae,$0E00   ; COLOR07
-    
-    dc.w      $01b0,$0000   ; COLOR00
-    dc.w      $01b2,$0222   ; COLOR01
-    dc.w      $01b4,$0444   ; COLOR02
-    dc.w      $01b6,$0666   ; COLOR03
-    dc.w      $01b8,$0888   ; COLOR04
-    dc.w      $01ba,$0AAA   ; COLOR05
-    dc.w      $01bc,$0CCC   ; COLOR06
-    dc.w      $01be,$0EEE   ; COLOR07
-    
+    dc.w      $0190,$0000   ; COLOR08
+    dc.w      $0192,$0020   ; COLOR09
+    dc.w      $0194,$0040   ; COLOR10
+    dc.w      $0196,$0060   ; COLOR11
+    dc.w      $0198,$0080   ; COLOR12
+    dc.w      $019a,$00A0   ; COLOR13
+    dc.w      $019c,$00C0   ; COLOR14
+    dc.w      $019e,$00E0   ; COLOR15
+    dc.w      $01a0,$0000   ; COLOR16
+    dc.w      $01a2,$0200   ; COLOR17
+    dc.w      $01a4,$0400   ; COLOR18
+    dc.w      $01a6,$0600   ; COLOR19
+    dc.w      $01a8,$0800   ; COLOR20
+    dc.w      $01aa,$0A00   ; COLOR21
+    dc.w      $01ac,$0C00   ; COLOR22
+    dc.w      $01ae,$0E00   ; COLOR23
+    dc.w      $01b0,$0000   ; COLOR24
+    dc.w      $01b2,$0222   ; COLOR25
+    dc.w      $01b4,$0444   ; COLOR26
+    dc.w      $01b6,$0666   ; COLOR27
+    dc.w      $01b8,$0888   ; COLOR28
+    dc.w      $01ba,$0AAA   ; COLOR29
+    dc.w      $01bc,$0CCC   ; COLOR30
+    dc.w      $01be,$0EEE   ; COLOR31
+    ;
     dc.w      $010c,$0011   ; BPLCON4
     dc.w      $008e,$2976   ; DIWSTRT
 CN0 dc.w      $0100,$1000   ; BPLCON0 <<< number of planes
@@ -268,42 +279,59 @@ CN0 dc.w      $0100,$1000   ; BPLCON0 <<< number of planes
     dc.w      $0102,$0055   ; BPLCON1
     dc.w      $0108,$0000   ; BPL1MOD
     dc.w      $010a,$0000   ; BPL2MOD
-
-S1H dc.w      $0120,$0000   ; SPR1PTR high
-S1L dc.w      $0122,$0000   ; SPR1PTR low
-S2H dc.w      $0124,$0000   ; SPR2PTR high
-S2L dc.w      $0126,$0000   ; SPR2PTR low
-S3H dc.w      $0128,$0000   ; SPR3PTR high
-S3L dc.w      $012a,$0000   ; SPR3PTR low
-S4H dc.w      $012c,$0000   ; SPR4PTR high
-S4L dc.w      $012e,$0000   ; SPR4PTR low
-S5H dc.w      $0130,$0000   ; SPR5PTR high
-S5L dc.w      $0132,$0000   ; SPR5PTR low
-S6H dc.w      $0134,$0000   ; SPR6PTR high
-S6L dc.w      $0136,$0000   ; SPR6PTR low
-S7H dc.w      $0138,$0000   ; SPR7PTR high
-S7L dc.w      $013a,$0000   ; SPR7PTR low
-S8H dc.w      $013c,$0000   ; SPR8PTR high
-S8L dc.w      $013e,$0000   ; SPR8PTR low
-    
-P1H dc.w      $00e0,$0000   ; BPL1PTR high
-P1L dc.w      $00e2,$0000   ; BPL1PTR low
-P2H dc.w      $00e4,$0000   ; BPL2PTR high
-P2L dc.w      $00e6,$0000   ; BPL2PTR low
-P3H dc.w      $00e8,$0000   ; BPL3PTR high
-P3L dc.w      $00ea,$0000   ; BPL3PTR low
-P4H dc.w      $00ec,$0000   ; BPL4PTR high
-P4L dc.w      $00ee,$0000   ; BPL4PTR low
-P5H dc.w      $00f0,$0000   ; BPL5PTR high
-P5L dc.w      $00f2,$0000   ; BPL5PTR low
-P6H dc.w      $00f4,$0000   ; BPL6PTR high
-P6L dc.w      $00f6,$0000   ; BPL6PTR low
-P7H dc.w      $00f8,$0000   ; BPL7PTR high
-P7L dc.w      $00fa,$0000   ; BPL7PTR low
-P8H dc.w      $00fc,$0000   ; BPL8PTR high
-P8L dc.w      $00fe,$0000   ; BPL8PTR low
-    
-    dc.w      $7001,$fffe,$0180,$0300 ; copperbar red
+    ;
+    dc.w      $0140,$0000   ; SPR0POS
+    dc.w      $0142,$0000   ; SPR0CTL
+    dc.w      $0148,$0000   ; SPR1POS
+    dc.w      $014A,$0000   ; SPR1CTL
+    dc.w      $0150,$0000   ; SPR2POS
+    dc.w      $0152,$0000   ; SPR2CTL
+    dc.w      $0158,$0000   ; SPR3POS
+    dc.w      $015A,$0000   ; SPR3CTL
+    dc.w      $0160,$0000   ; SPR4POS
+    dc.w      $0162,$0000   ; SPR4CTL
+    dc.w      $0168,$0000   ; SPR5POS
+    dc.w      $016A,$0000   ; SPR5CTL
+    dc.w      $0170,$0000   ; SPR6POS
+    dc.w      $0172,$0000   ; SPR6CTL
+    dc.w      $0178,$0000   ; SPR7POS
+    dc.w      $017A,$0000   ; SPR7CTL
+    ;
+S1H dc.w      $0120,$0000   ; SPR1PTH
+S1L dc.w      $0122,$0000   ; SPR1PTL
+S2H dc.w      $0124,$0000   ; SPR2PTH
+S2L dc.w      $0126,$0000   ; SPR2PTL
+S3H dc.w      $0128,$0000   ; SPR3PTH
+S3L dc.w      $012a,$0000   ; SPR3PTL
+S4H dc.w      $012c,$0000   ; SPR4PTH
+S4L dc.w      $012e,$0000   ; SPR4PTL
+S5H dc.w      $0130,$0000   ; SPR5PTH
+S5L dc.w      $0132,$0000   ; SPR5PTL
+S6H dc.w      $0134,$0000   ; SPR6PTH
+S6L dc.w      $0136,$0000   ; SPR6PTL
+S7H dc.w      $0138,$0000   ; SPR7PTH
+S7L dc.w      $013a,$0000   ; SPR7PTL
+S8H dc.w      $013c,$0000   ; SPR8PTH
+S8L dc.w      $013e,$0000   ; SPR8PTL
+    ;
+P1H dc.w      $00e0,$0000   ; BPL1PTH
+P1L dc.w      $00e2,$0000   ; BPL1PTL
+P2H dc.w      $00e4,$0000   ; BPL2PTH
+P2L dc.w      $00e6,$0000   ; BPL2PTL
+P3H dc.w      $00e8,$0000   ; BPL3PTH
+P3L dc.w      $00ea,$0000   ; BPL3PTL
+P4H dc.w      $00ec,$0000   ; BPL4PTH
+P4L dc.w      $00ee,$0000   ; BPL4PTL
+P5H dc.w      $00f0,$0000   ; BPL5PTH
+P5L dc.w      $00f2,$0000   ; BPL5PTL
+P6H dc.w      $00f4,$0000   ; BPL6PTH
+P6L dc.w      $00f6,$0000   ; BPL6PTL
+P7H dc.w      $00f8,$0000   ; BPL7PTH
+P7L dc.w      $00fa,$0000   ; BPL7PTL
+P8H dc.w      $00fc,$0000   ; BPL8PTH
+P8L dc.w      $00fe,$0000   ; BPL8PTL
+    ;
+    dc.w      $7001,$fffe,$0180,$0300 ; CopperBar RED
     dc.w      $7101,$fffe,$0180,$0600
     dc.w      $7201,$fffe,$0180,$0900
     dc.w      $7301,$fffe,$0180,$0b00
@@ -315,21 +343,21 @@ P8L dc.w      $00fe,$0000   ; BPL8PTR low
     dc.w      $7d01,$fffe,$0180,$0600
     dc.w      $7e01,$fffe,$0180,$0300
     dc.w      $7f01,$fffe,$0180,$0000
-
-    dc.w      $8001,$fffe,$0180,$0333 ; copperbar grey
-    dc.w      $8101,$fffe,$0180,$0666
-    dc.w      $8201,$fffe,$0180,$0999
-    dc.w      $8301,$fffe,$0180,$0bbb
-    dc.w      $8401,$fffe,$0180,$0ddd
-    dc.w      $8501,$fffe,$0180,$0fff
-    dc.w      $8a01,$fffe,$0180,$0ddd
-    dc.w      $8b01,$fffe,$0180,$0bbb
-    dc.w      $8c01,$fffe,$0180,$0999
-    dc.w      $8d01,$fffe,$0180,$0666
-    dc.w      $8e01,$fffe,$0180,$0333
+    ;
+    dc.w      $8001,$fffe,$0180,$0030 ; CopperBar GREEN
+    dc.w      $8101,$fffe,$0180,$0060
+    dc.w      $8201,$fffe,$0180,$0090
+    dc.w      $8301,$fffe,$0180,$00b0
+    dc.w      $8401,$fffe,$0180,$00d0
+    dc.w      $8501,$fffe,$0180,$00f0
+    dc.w      $8a01,$fffe,$0180,$00d0
+    dc.w      $8b01,$fffe,$0180,$00b0
+    dc.w      $8c01,$fffe,$0180,$0090
+    dc.w      $8d01,$fffe,$0180,$0060
+    dc.w      $8e01,$fffe,$0180,$0030
     dc.w      $8f01,$fffe,$0180,$0000
-    
-    dc.w      $9001,$fffe,$0180,$0003 ; copperbar blue
+    ;
+    dc.w      $9001,$fffe,$0180,$0003 ; CopperBar BLUE
     dc.w      $9101,$fffe,$0180,$0006
     dc.w      $9201,$fffe,$0180,$0009
     dc.w      $9301,$fffe,$0180,$000b
@@ -341,9 +369,15 @@ P8L dc.w      $00fe,$0000   ; BPL8PTR low
     dc.w      $9d01,$fffe,$0180,$0006
     dc.w      $9e01,$fffe,$0180,$0003
     dc.w      $9f01,$fffe,$0180,$0000
-    
+    ;
+    dc.w      $01fc,$0002   ; FMODE
     dc.w      $ffff,$fffe   ; WAIT
     dc.w      $ffff,$fffe   ; WAIT
+
+    EVEN
+_USERSPRITE1:
+    dc.w      $0000
+    dc.w      $0000
 
     EVEN
 _USERPLANE1:
